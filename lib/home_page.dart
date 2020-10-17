f087d09619e3508ed07c9894a610945128f1c132
UltimateDip <68418035+UltimateDip@users.noreply.github.com>
2020-10-17 18:10:33 +0530

    updated the location issue

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -31,12 +31,22 @@ class _HomePageState extends State<HomePage> {
   }
 
   void updateContainerUi(dynamic weatherData) {
+    setState(() {
+      if (weatherData == null) {
+        temperature = 0;
+        humidity = 0;
+        description = 0;
+        windSpeed = 0;
+        cityName = "Please check your internet connection or turn on location";
+        return;
+      }
+    });
+
     temperature = weatherData['main']['temp'];
     humidity = weatherData['main']['humidity'];
     description = weatherData['weather'][0]['description'];
     windSpeed = weatherData['wind']['speed'];
     cityName = weatherData['name'];
-
 
     // To upload data
     // final database = FirebaseDatabase.instance;
--- lib/models/location.dart
+++ lib/models/location.dart
@@ -4,13 +4,24 @@ class Location {
   double latitude;
   double longitude;
   Future <void> getCurrentLocation() async {
-    try{
-      Position position = await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    try {
+      Position position = await getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low,
+      );
       latitude = position.latitude;
       longitude = position.longitude;
-    }
-    catch (e){
+    } catch (e) {
       print(e);
+      //requestPermission();
     }
   }
+
+// Future<void> getPermission() async{
+//
+//   LocationPermission permission = await checkPermission();
+//
+//   if(permission == LocationPermission.denied ){
+//     await openLocationSettings();
+//   }
+// }
 }
